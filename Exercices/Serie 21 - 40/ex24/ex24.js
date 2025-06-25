fc70d22b9b589269fc6878b3b38186c935ea6008
JeanMichelConti <jean-michelconti@hotmail.com>
2025-06-25 19:31:43 +0200

    Update ex24.js

--- Exercices/Serie 21 - 40/ex24/ex24.js
+++ Exercices/Serie 21 - 40/ex24/ex24.js
@@ -4,4 +4,14 @@ let minuculas = document.getElementById('minuculas')
 let mayusculas = document.getElementById('mayusculas')
 let number = document.getElementById('number')
 
-// Quand l'utilisateur clique sur le champ du mot de passe, affiche la boîte de message
+// Quand l'utilisateur clique sur le input du mot de passe, affiche la boîte de message
+passwordInput.onfocus = () => {document.getElementById('message').style.display = 'block'}
+
+// Quand l'utilisateur clique en dehors du input du mot de passe, affiche la boîte de message
+passwordInput.onblur = () => { document.getElementById('message').style.display = 'none' }
+
+// Quand l'utilisateur commence a écrire quelque chose dans l'input du mot de passe
+passwordInput.onkeyup = () => {
+  // Validation des minuscules 
+  let minuculas = /[a-z]
+}
