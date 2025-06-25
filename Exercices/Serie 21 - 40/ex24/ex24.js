107ae8cca7ec4e31715e92a628bb9bcf38b37aaf
JeanMichelConti <jean-michelconti@hotmail.com>
2025-06-25 19:18:47 +0200

    Update ex24.html & ex24.js
    Modifs nom variables dans le fichier JS et les id dans l'HTML

--- Exercices/Serie 21 - 40/ex24/ex24.js
+++ Exercices/Serie 21 - 40/ex24/ex24.js
@@ -1,5 +1,7 @@
-let userName = document.getElementById('username')
-let password = document.getElementById('password')
+let userNameInput = document.getElementById('usernameInput')
+let passwordInput = document.getElementById('passwordInput')
 let minuculas = document.getElementById('minuculas')
 let mayusculas = document.getElementById('mayusculas')
 let number = document.getElementById('number')
+
+// Quand l'utilisateur clique sur le champ du mot de passe, affiche la boîte de message
