f4c22372c0a93ed39677ca8e531f034dd0b748d7
JeanMichelConti <jean-michelconti@hotmail.com>
2025-06-26 20:44:12 +0200

    Update ex24-v2.js

--- Exercices/Serie 21 - 40/ex24/V2/ex24-v2.js
+++ Exercices/Serie 21 - 40/ex24/V2/ex24-v2.js
@@ -2,7 +2,24 @@ function validate(password) {
   const minLength = /^.{8}$/
   const haveUpperCase = /[A-Z]/
   const haveLowerCase = /[a-z]/
-  const haveLNumber = /\d/
+  const haveNumber = /\d/
   const haveSpecialC = / [!@#$%^&*()":{}<>]/
   
+  if (!minLength.test(password)) {
+    return 'La contraseña debe tener al menos 8 caracteres'
+  }
+  if (!haveUpperCase.test(password)) {
+    return 'La contraseña debe tener al menos una mayuscula'
+  }
+  if (!haveLowerCase.test(password)) {
+    return 'La contraseña debe tener al menos una minuscula'
+  }
+  if (!haveNumber.test(password)) {
+    return 'La contraseña debe tener al menos un numero'
+  }
+  if (!haveSpecialC.test(password)) {
+    return 'La contraseña debe tener al menos un carácter especial'
+  }
+
+  return 'La contraseña es valida'
 }
