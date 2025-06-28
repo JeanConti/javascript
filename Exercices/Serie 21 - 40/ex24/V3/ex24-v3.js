bb5a0fd6f4ae99369145d120e28bd6d4e522df0e
JeanMichelConti <jean-michelconti@hotmail.com>
2025-06-28 11:52:40 +0200

    Summary

---
+++ Exercices/Serie 21 - 40/ex24/V3/ex24-v3.js
@@ -0,0 +1,9 @@
+const passwordInput = document.getElementById('password')
+const togglePassword = document.getElementById('toggle-password')
+const icon = document.querySelector(i)
+const message = document.getElementById('message')
+
+function togglePasswordVisibible() {
+
+  
+}
